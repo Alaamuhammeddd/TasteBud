c43273a92ad751601c8aa2e521ce0a703da92fc4
Alaamuhammeddd <alaamuhammeddd17@gmail.com>
2024-10-01 13:30:48 +0300

    Starter Book Table

---
+++ src/pages/TableBooking/book-a-table/book-a-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-a-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './book-a-table.component.html',
+  styleUrl: './book-a-table.component.css'
+})
+export class BookATableComponent {
+
+}
